347b9dc0e9dc32180a4d5a88cff8cfee472d58e8
Dev Tsubaki <dev.tsubaki@gmail.com>
2013-07-21 14:27:26 +0900

    complete lesson

--- index.php
+++ index.php
@@ -3,6 +3,50 @@
 require_once('config.php');
 require_once('functions.php');
 
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    // 投稿前
+    
+    // CSRF対策
+    if (!isset($_SESSION['token'])) {
+        $_SESSION['token'] = sha1(uniqid(mt_rand(), true));
+    }
+    
+} else {
+    // 投稿後
+    if (empty($_POST['token']) || $_POST['token'] != $_SESSION['token']) {
+        echo "不正な操作です！";
+        exit;
+    }
+
+    // エラーチェック
+    if (!in_array($_POST['answer'], array(1, 2, 3, 4))) {
+        $err = "写真を選択してください！";
+    }
+    
+    if (empty($err)) {
+        $dbh = connectDb();
+        $sql = "insert into answers 
+                (answer, remote_addr, user_agent, answer_date, created, modified) 
+                values 
+                (:answer, :remote_addr, :user_agent, :answer_date, now(), now())";
+        $stmt = $dbh->prepare($sql);
+        $params = array(
+            ":answer" => $_POST['answer'],
+            ":remote_addr" => $_SERVER['REMOTE_ADDR'],
+            ":user_agent" => $_SERVER['HTTP_USER_AGENT'],
+            ":answer_date" => date("Y-m-d")
+        );
+        
+        if ($stmt->execute($params)) {
+            $msg = "投票ありがとうございました！";
+        } else {
+            $err = "投票は1日1回までです！";
+        }
+        
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -15,6 +59,9 @@ require_once('functions.php');
     .selected {
         border:4px solid red;
     }
+	.candidate {
+		width: 300px;
+	}
     </style>
 </head>
 <body>
@@ -26,7 +73,9 @@ require_once('functions.php');
 <img src="food4.jpg" class="candidate" data-id="4">
 <p><input type="submit" value="投票する！"></p>
 <input type="hidden" id="answer" name="answer" value="">
+<input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
 </form>
+<p><a href="result.php">結果を見る</a></p>
 <script>
 $(function() {
     $('.candidate').click(function() {
